4f1c06a17f16fb7850bd2441287acc76fbd00537
Batuhan <sfs561@york.ac.uk>
2026-04-18 14:11:51 +0000

    testing new functions

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from database import create_tables, add_patient, get_all_patients
+from database import create_tables, add_patient, add_doctor, get_all_patients, get_all_doctors
 
 def main():
     create_tables()
+
     add_patient("Batuhan", 22, "07123456789")
+    add_doctor("Dr Smith", "Cardiology")
+
+    print("Patients:")
     print(get_all_patients())
+
+    print("Doctors:")
+    print(get_all_doctors())
 
 if __name__ == "__main__":
     main()
